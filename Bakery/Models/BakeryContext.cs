eff0980e17ab38470f7591a08a773a7985edba37
Kate Skorija <kateskorija@gmail.com>
2020-08-14 08:51:10 -0700

    create applicationUser and bakeryContext models

---
+++ Bakery/Models/BakeryContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Bakery.Models
+{
+  public class BakeryContext : IdentityDbContext<ApplicationUser>
+  {
+    public virtual DbSet<Flavor> Flavors { get; set; }
+    public virtual DbSet<Treat> Treats { get; set; }
+    public DbSet<FlavorTreat> FlavorTreat { get; set; }
+    public BakeryContext(DbContextOptions options) : base(options) { }
+  }
+}
