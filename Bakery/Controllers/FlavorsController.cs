5788b1b1537d8ebd42fce5b642ba1d8be78b4d6c
Kate Skorija <kateskorija@gmail.com>
2020-08-14 09:47:10 -0700

    create details route and view for flavors controller

--- Bakery/Controllers/FlavorsController.cs
+++ Bakery/Controllers/FlavorsController.cs
@@ -29,5 +29,13 @@ namespace Bakery.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+    public ActionResult Details(int id)
+    {
+      var thisFlavor = _db.Flavors
+          .Include(flavor => flavor.Treats)
+          .ThenInclude(join => join.Treat)
+          .FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
   }
 }
