7a357a802d515b26490441fa86dd1fd65f69d9b3
Kate Skorija <kateskorija@gmail.com>
2020-08-14 11:17:40 -0700

    add login and register routes and views, make home controller authorized

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -1,14 +1,20 @@
 using Bakery.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 
+
 namespace Bakery.Controllers
 {
+  [Authorize]
   public class HomeController : Controller
   {
     private readonly BakeryContext _db;
-    public HomeController(BakeryContext db)
+    private readonly UserManager<ApplicationUser> _userManager;
+    public HomeController(UserManager<ApplicationUser> userManager, BakeryContext db)
     {
+      _userManager = userManager;
       _db = db;
     }
 
