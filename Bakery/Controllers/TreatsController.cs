3d13a8881c75b3f08615c6d06b3cc20d874f2d69
Kate Skorija <kateskorija@gmail.com>
2020-08-14 10:17:45 -0700

    add create routes and view for treats controller

--- Bakery/Controllers/TreatsController.cs
+++ Bakery/Controllers/TreatsController.cs
@@ -18,5 +18,16 @@ namespace Bakery.Controllers
     {
       return View(_db.Treats.ToList());
     }
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Treat treat)
+    {
+      _db.Treats.Add(treat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
